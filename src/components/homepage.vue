688d389be29a42f8ba014df07a0a9a444dcc7e55
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-20 16:41:38 +0430

    selling festival

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -5,6 +5,24 @@
     <productSlider/>
 
     <suggestioncarousel/>
+
+    <b-container>
+        <div class="d-flex justify-content-between align-items-center">
+          <a href="#">
+            <h4 class="text-right">
+            جدیدترین جشنواره های فروش  
+            </h4>   
+          </a>
+
+          <a href="#" class="text-left">
+            <span>
+              مشاهده همه />
+            </span>  
+          </a>
+        </div>
+    </b-container>
+
+    <sellingfestival/>
   </div>
 </template>
 
@@ -12,17 +30,26 @@
 import siteheader from '@/components/header/siteheader.vue'
 import productSlider from '@/components/productSlider.vue'
 import suggestioncarousel from '@/components/suggestions/suggestionCarousel.vue'
+import sellingfestival from '@/components/sellingFestival/sellingFestival.vue'
 
 export default {
   name: "home",
   components : {
     siteheader,
     productSlider,
-    suggestioncarousel
+    suggestioncarousel,
+    sellingfestival
   },
   props: {
     msg: String,
   },
+  data () {
+    return {
+      sections: [
+        'جدیدترین جشنواره های فروش'
+      ]
+    }
+  }
 };
 </script>
 
---
+++ src/components/sellingFestival/sellingFestival.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        
+      </b-row>
+    </b-container>
+    <carousel :perPageCustom="[[0, 1], [768, 2], [1024,4]]" :paginationEnabled="false">
+      <slide>
+        <b-container fluid class="bg-white">
+          <b-row>
+            <h5>
+              کمپین کامپوره
+            </h5>
+            
+            <b-badge pill variant="danger" class="p-2">
+               تخفیف 
+            </b-badge>
+          </b-row>
+          <b-row>
+            <b-col cols="6">
+              <div>
+                <img src="https://img.beroozresaan.com/unsafe/190x190/filters:format(webp)/files/shop/product/326fde3bffb24c05ad32da99e4aebf92.jpg" alt="">
+                <p class="text-right">
+                  jjjj
+                </p>
+
+                <div class="mb-2 text-left px-3">
+                  <div class="text-muted">
+                    <span class="discount-value position-relative px-2">
+                      jjj
+                    </span>
+                    
+                    <div class="font-weight-bold text-left px-3">
+                      jj
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </b-col>
+
+            <b-col cols="6">
+              <b-card
+                img-src="https://img.beroozresaan.com/unsafe/190x190/filters:format(webp)/files/shop/product/326fde3bffb24c05ad32da99e4aebf92.jpg"
+                img-alt="suggestedProduct.name"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+              >
+                <b-card-text class="text-right">
+                  jjj
+                </b-card-text>
+
+                <div class="mb-2 text-left px-3">
+                  <div class="text-muted">
+                    <span class="discount-value position-relative px-2">
+                      jjj
+                    </span>
+                    
+                    <div class="font-weight-bold text-left px-3">
+                      jjjj
+                    </div>
+                  </div>
+                </div>
+              </b-card>
+            </b-col>
+          </b-row>
+        </b-container>
+      </slide>
+    </carousel>
+  </div>
+</template>
