707f6eef6d9cb3a1d2ab81ede934fe4a05b9a36d
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-22 04:24:23 +0430

    first gallery added

--- src/components/category/category.vue
+++ src/components/category/category.vue
@@ -1,24 +1,27 @@
 <template>
   <carousel :perPageCustom="[[0, 1], [768, 2], [1024,5]]" :paginationEnabled="false">
     <slide>
-        Slide 1 Content
-    </slide>
-
-    <slide>
-        Slide 2 Content
-    </slide>
-
-    <slide>
-        Slide 2 Content
-    </slide>
-
-    <slide>
         <b-img src="https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/widget/item/29d783e415f54c2cac123aed5828e57f.png">
         </b-img>
     </slide>
 
     <slide>
         <b-img src="https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/widget/item/1b97d97d73e240b282bef4bd724e9920.png">
+        </b-img>
+    </slide>
+    
+    <slide>
+        <b-img src="https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/widget/item/592dafc9e45248bdaba3abff1ade2ef6.png">
+        </b-img>
+    </slide>
+
+    <slide>
+        <b-img src="https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/widget/item/907a9ed20ee94559976f8fd980f0a82b.png">
+        </b-img>
+    </slide>
+
+    <slide>
+        <b-img src="https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/widget/item/a402469d5cfd48d3908c6f49d6ec38f2.png">
         </b-img>
     </slide>
 </carousel>
--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -28,6 +28,8 @@
 
     <category/>
 
+    <firstProductGallery/>
+
     <productGallery/>
 
     <sitefooter/>
@@ -40,6 +42,7 @@ import productSlider from '@/components/productSlider.vue'
 import suggestioncarousel from '@/components/suggestions/suggestionCarousel.vue'
 import sellingfestival from '@/components/sellingFestival/sellingFestival.vue'
 import category from '@/components/category/category.vue'
+import firstProductGallery from '@/components/productGallery/firstProductGallery.vue'
 import productGallery from '@/components/productGallery/productGallery.vue'
 import sitefooter from '@/components/footer/sitefooter.vue'
 
@@ -51,6 +54,7 @@ export default {
     suggestioncarousel,
     sellingfestival,
     category,
+    firstProductGallery,
     productGallery,
     sitefooter
   },
