8199e4cf7978b606096688c8efdaeda1a3898988
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-22 12:43:30 +0430

    carousels added

--- src/components/carousel/carousel.vue
+++ src/components/carousel/carousel.vue
@@ -1,11 +1,5 @@
 <template>
-  <b-container>
-    <carousel :perPageCustom="[[0, 1], [768, 2], [1024,5]]" :paginationEnabled="false" class="my-3 orange-border-top">
-      <slide v-for="product in firstcarousel" :key="product.name">
-        <product :product="product" :name="product.name"/>
-      </slide>
-    </carousel>
-  </b-container>
+
 </template>
 
 
@@ -16,46 +10,5 @@ export default {
   components: {
     product
   },
-  data () {
-    return {
-      firstcarousel: [
-        { 
-          name: 'سبزی آش خرد شده دکتر بیژن  400 گرمی',
-          price: 27000,
-          discount: '30%',
-          afterdiscount: 18000,
-          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/9589d96bf1be42db9121ae288cdcd372.jpg'
-        },
-        { 
-          name: 'روغن کلزا پیور 500 سی سی',
-          price: 27000,
-          discount: '30%',
-          afterdiscount: 18000,
-          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/42622c9445cb4185a449fdc5f5310867.jpg'
-        },
-        { 
-          name: 'گوشت خورشتی گوساله ممتاز روناک پروتئین 800 گرمی',
-          price: 27000,
-          discount: '30%',
-          afterdiscount: 18000,
-          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/b2bc64448d054775b6a2e2c02af57137.jpg'
-        },
-        { 
-          name: 'پنیر پیتزای پروسس دو رنگ ناژوان 1000 گرمی',
-          price: 27000,
-          discount: '30%',
-          afterdiscount: 18000,
-          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/7c1059a92a234e37bf48d97004d65b9b.jpg'
-        },
-        { 
-          name: 'سبزی آش خرد شده دکتر بیژن  400 گرمی',
-          price: 27000,
-          discount: '30%',
-          afterdiscount: 18000,
-          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/9589d96bf1be42db9121ae288cdcd372.jpg'
-        }
-      ]
-    }
-  }
 }
 </script>
--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -28,33 +28,49 @@
 
     <category/>
 
-    <carousel/>
+    <b-container>
+      <carousel :perPageCustom="[[0, 1], [768, 2], [1024,5]]" :paginationEnabled="false" class="my-3 orange-border-top">
+        <slide v-for="product in firstcarousel" :key="product.id">
+          <product :product="product" :name="product.name"/>
+        </slide>
+      </carousel>
+    </b-container>
 
     <firstProductGallery/>
 
-  <b-container>
-    <div class="d-flex justify-content-between align-items-center">
-      <a href="#">
-        <h4 class="text-right">
-        جدیدترین جشنواره های فروش  
-        </h4>   
-      </a>
+    <b-container>
+      <div class="d-flex justify-content-between align-items-center">
+        <a href="#">
+          <h4 class="text-right">
+          جدیدترین جشنواره های فروش  
+          </h4>   
+        </a>
 
-      <a href="#" class="text-left">
-        <span>
-          مشاهده همه />
-        </span>  
-      </a>
-    </div>
-  </b-container>
+        <a href="#" class="text-left">
+          <span>
+            مشاهده همه />
+          </span>  
+        </a>
+      </div>
+    </b-container>
 
-    <carousel/>
-
+    <b-container>
+      <carousel :perPageCustom="[[0, 1], [768, 2], [1024,5]]" :paginationEnabled="false" class="my-3 orange-border-top">
+        <slide v-for="product in secondcarousel" :key="product.id">
+          <product :product="product" :name="product.name"/>
+        </slide>
+      </carousel>
+    </b-container>
 
     <productGallery/>
 
-    <carousel/>
-
+    <b-container>
+      <carousel :perPageCustom="[[0, 1], [768, 2], [1024,5]]" :paginationEnabled="false" class="my-3 orange-border-top">
+        <slide v-for="product in thirdcarousel" :key="product.id">
+          <product :product="product" :name="product.name"/>
+        </slide>
+      </carousel>
+    </b-container>
 
     <sitefooter/>
   </div>
@@ -66,7 +82,7 @@ import productSlider from '@/components/productSlider.vue'
 import suggestioncarousel from '@/components/suggestions/suggestionCarousel.vue'
 import sellingfestival from '@/components/sellingFestival/sellingFestival.vue'
 import category from '@/components/category/category.vue'
-import carousel from '@/components/carousel/carousel.vue'
+import product from '@/components/carousel/product.vue'
 import firstProductGallery from '@/components/productGallery/firstProductGallery.vue'
 import productGallery from '@/components/productGallery/productGallery.vue'
 import sitefooter from '@/components/footer/sitefooter.vue'
@@ -79,7 +95,7 @@ export default {
     suggestioncarousel,
     sellingfestival,
     category,
-    carousel,
+    product,
     firstProductGallery,
     productGallery,
     sitefooter
@@ -89,8 +105,163 @@ export default {
   },
   data () {
     return {
-      sections: [
-        'جدیدترین جشنواره های فروش'
+      firstcarousel: [
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'سبزی آش خرد شده دکتر بیژن  400 گرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/9589d96bf1be42db9121ae288cdcd372.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'روغن کلزا پیور 500 سی سی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/42622c9445cb4185a449fdc5f5310867.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'گوشت خورشتی گوساله ممتاز روناک پروتئین 800 گرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/b2bc64448d054775b6a2e2c02af57137.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'پنیر پیتزای پروسس دو رنگ ناژوان 1000 گرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/7c1059a92a234e37bf48d97004d65b9b.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'سبزی آش خرد شده دکتر بیژن  400 گرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/315x315/filters:format(webp)/files/shop/product/9589d96bf1be42db9121ae288cdcd372.jpg'
+        }
+      ],
+      secondcarousel: [
+       { 
+         id: String(Math.floor(Math.random() * 999999999)),
+          name: 'دستمال جذب روغن غذا برند کاتوس',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/bc68a45a851f4ce292cbf4fda9672cc0.jpg'
+        },
+       { 
+         id: String(Math.floor(Math.random() * 999999999)),
+          name: 'لیف حمام ابریشمی بلند برند کاتوس',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/7eb3e61e9f9647ecab706b52363b3a66.jpg'
+        },
+       { 
+         id: String(Math.floor(Math.random() * 999999999)),
+          name: 'لیف حمام کلاسیک برند کاتوس',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/57cd68c117ee4f51a6c9c5f9ea67feed.jpg'
+        },
+       { 
+         id: String(Math.floor(Math.random() * 999999999)),
+          name: 'روکش بهداشتی توالت فرنگی رد شونده برند کاتوس',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/08403593a01a4891ae55fc8d66a0ca82.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'دستمال پاورکلین برند کاتوس 4 عددی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/5f36d80d29154fdb8b4eac18e2c43d40.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'دستمال پاورکلین برند کاتوس 4 عددی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/5f36d80d29154fdb8b4eac18e2c43d40.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'دستمال پاورکلین برند کاتوس 4 عددی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/5f36d80d29154fdb8b4eac18e2c43d40.jpg'
+        }
+      ],
+      thirdcarousel: [
+       { 
+         id: String(Math.floor(Math.random() * 999999999)),
+          name: 'ران گوسفند کشتار داخلی مهیا پروتئین 2 کیلوگرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/f9d7da976e524c7cae05f9d573c668c2.jpg'
+        },
+       { 
+         id: String(Math.floor(Math.random() * 999999999)),
+          name: 'ران گوسفندی وارداتی رزا 2 کیلوگرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/013f1e29ad0f48a38480416e17d66d86.jpg'
+        },
+       { 
+         id: String(Math.floor(Math.random() * 999999999)),
+          name: 'راسته گوسفند بی استخوان کشتار داخلی مهیا پروتئین 1 کیلوگرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/82e36a214ed84cb6a01c563a2f7b7fad.jpg'
+        },
+       { 
+         id: String(Math.floor(Math.random() * 999999999)),
+          name: 'ماهیچه گوسفندی آریا بهار پروتئین 1 کیلوگرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/958bae714d454772adf48e9f153fcdf3.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'گوشت ران گوسفند آریا بهار پروتئین 1 کیلوگرمی ',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/f9d7da976e524c7cae05f9d573c668c2.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'ماهیچه گوسفندی آریا بهار پروتئین 1 کیلوگرمی',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/958bae714d454772adf48e9f153fcdf3.jpg'
+        },
+        { 
+          id: String(Math.floor(Math.random() * 999999999)),
+          name: 'گوشت ران گوسفند آریا بهار پروتئین 1 کیلوگرمی ',
+          price: 27000,
+          discount: '30%',
+          afterdiscount: 18000,
+          src: 'https://img.beroozresaan.com/unsafe/270x270/filters:format(webp)/files/shop/product/f9d7da976e524c7cae05f9d573c668c2.jpg'
+        }
       ]
     }
   }
