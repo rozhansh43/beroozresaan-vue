d50a6e57eeab55401b6500e95c3b22342b4c6e9e
rozhan sheikholmoluki <rozhansheikholmoluki@gmail.com>
2022-03-06 17:00:48 +0330

    local storage added

--- src/components/productCategory.vue
+++ src/components/productCategory.vue
@@ -1,133 +1,137 @@
 <template>
-  <div class="domain_list">
-    <div class="action">
-       <span @click="actionToggle();" id="demo" class="m-5 p-2">
-         < لیست دامنه ها 
-        </span>
-        
-        <div class="domian_carousel d-flex"> 
-            <carousel :perPageCustom="[[480, 2], [768, 4]]" class="w-100">
-                <slide 
-                    v-for="slide in slides"
-                    :key="slide.id"
-                    class="carousel_slide"
+  <div class="container">
+      
+        My name is {{name}}
+    <div class="domain_list row mt-5">
+      <div class="col-2">
+          
+      </div>
+      <div class="col-8">
+        <transition name="slide-fade">
+            <div v-if="show_domains" class="domian_carousel d-flex">
+            <span class="add_domain px-3 py-2 m-auto"> افزودن دامنه + </span>
+
+            <carousel
+                :perPageCustom="[
+                [480, 2],
+                [768, 4],
+                ]"
+                class="w-100  my-2"
+            >
+                <slide
+                v-for="slide in slides"
+                :key="slide.id"
+                class="carousel_slide mx-1 px-3 py-2"
                 >
-                    <div 
-                     @click="getName(slide.name)"
-                    >
-                       {{ slide.name}}  
-                    </div>
+                <div @click="getName(slide.name)">
+                    {{ slide.name }}
+                </div>
                 </slide>
             </carousel>
-            
-            <span class="add_domain">
-                افزودن دامنه +
-            </span>
-            
-        </div>
-    </div>       
+            </div>
+        </transition>
+      </div>
+
+      <div class="col-2 align-items-center d-flex">
+        <span @click="domainList" id="demo" class="p-2 action">
+          < لیست دامنه ها
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            slides: [
-                {
-                    id:1,
-                    name: "slide one"
-                },
-                {
-                    id:2,
-                    name: "slide two"
-                },
-                {
-                    id:3,
-                    name: "slide three"
-                },
-                {
-                    id:4,
-                    name: "slide four"
-                },
-            ]
-        }
-    },
-    methods: {
-        actionToggle() {
-            const action = document.querySelector('.action');
-            action.classList.toggle('active');
-            var x = document.getElementById("demo");
-            if (x.innerHTML === "X") {
-                x.innerHTML = "< لیست دامنه ها"
-            }
-            if (x.innerHTML = "< لیست دامنه ها)")
-            {
-                x.innerHTML = "X";
-            } else {
-                x.innerHTML = "< لیست دامنه ها"
-            }
+  data() {
+    return {
+      slides: [
+        {
+          id: 1,
+          name: "slide one",
         },
-        getName(slide_name) {
-            var y = document.getElementById("demo");
-            console.log("ihnujhu");
-            y.innerHTML = slide_name + "<"
+        {
+          id: 2,
+          name: "slide two",
         },
+        {
+          id: 3,
+          name: "slide three",
+        },
+        {
+          id: 4,
+          name: "slide four",
+        },
+      ],
+      show_domains: false,
+      name: ''
+    };
+  },
+   mounted() {
+    if (localStorage.slide_name) {
+      this.name = localStorage.slide_name;
     }
-}
+  },
+  methods: {
+    actionToggle() {
+      const action = document.querySelector(".action");
+      action.classList.toggle("active");
+      var x = document.getElementById("demo");
+      if (x.innerHTML === "X") {
+        x.innerHTML = "< لیست دامنه ها";
+      }
+      if ((x.innerHTML = "< لیست دامنه ها)")) {
+        x.innerHTML = "X";
+      } else {
+        x.innerHTML = "< لیست دامنه ها";
+      }
+    },
+    getName(slide_name) {
+      var y = document.getElementById("demo");
+      console.log("ihnujhu");
+      y.innerHTML = ">" + slide_name ;
+      localStorage.slide_name = slide_name 
+      this.show_domains = false
+    },
+    domainList() {
+      this.show_domains = !this.show_domains;
+    },
+  },
+};
 </script>
 
 <style scoped>
 .add_domain {
-  background: #6da9c4;
+  background: #9ad2eb;
   color: #fff;
   border-radius: 40px;
+  display: block ruby;
 }
-
-.domain_list {
-    display: flex;
-    justify-content: end;
-}
-.action #demo {
-  width: fit-content;
+.action {
   border-radius: 40px;
   cursor: pointer;
-  box-shadow: 0 5px 5px rgba(0,0,0,0.1);
-  display: flex;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
   background: #6da9c4;
   color: #fff;
-  justify-content: center;
-  align-items: center;
-  transition: .3s ease-in-out;
-}
-.action .domian_carousel {
-  position: absolute;
-  left: 170px;
-  top: 23px;
-  width: 100%;
-  padding: 20px;
-  border-radius: 20px;
-  opacity: 0;
-  visibility: hidden;
-  transition: .5s;
+  transition: 0.3s ease-in-out;
 }
 
-.action.active .domian_carousel {
-  opacity: 1;
-  visibility: visible;
-  transition: .3s;
-}
-
-.action .domian_carousel .carousel_slide {
-  list-style:none;
-  text-decoration: none;
+.domian_carousel .carousel_slide {
   background: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 7px 0;
-  margin: 5px;
   border-radius: 40px;
-  box-shadow: 0 5px 5px rgba(0,0,0,0.1); 
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
+}
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.5, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(-100px);
+  opacity: 0;
 }
 </style>
