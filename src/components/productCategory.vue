091495555858dc678784703ba6530c5ed2539070
rozhan sheikholmoluki <rozhansheikholmoluki@gmail.com>
2022-03-07 11:30:17 +0330

    responsive fixed

--- src/components/productCategory.vue
+++ src/components/productCategory.vue
@@ -1,38 +1,64 @@
 <template>
-  <div class="container">
-    <div class="domain_list row mt-5">
-      <div class="col-2 align-items-center d-flex justify-content-end">
-        <span @click="domainList" id="demo" class="p-2 action">
-          لیست دامنه ها >
-        </span>
-      </div>
-      <div class="col-8">
-        <transition name="slide-fade">
-          <div v-if="show_domains" class="domian_carousel d-flex">
-            <carousel
-              :paginationEnabled="false"
-              :perPageCustom="[
-                [480, 2],
-                [768, 4],
-              ]"
-              class="w-100"
-            >
-              <slide
-                v-for="slide in slides"
-                :key="slide.id"
-                class="carousel_slide px-3 py-2"
-              >
-                <div @click="getName(slide.name)">
-                  {{ slide.name }}
-                </div>
-              </slide>
-            </carousel>
-            
-            <span class="add_domain px-3 py-2 mx-2"> افزودن دامنه + </span>
+  <div>
+    <div class="text-right d-md-none">
+      <b-dropdown
+        size="lg"
+        variant="link"
+        toggle-class="text-decoration-none"
+        no-caret
+      >
+        <template #button-content>
+          <div class="bg-blue radius-40 py-1 px-3">
+            <img src="@/assets/arrow-down.png" class="bg-lightgray radius-40 p-1 mx-1" width="15" alt="">
+            <span id="demo1" class="h6 text-light">لیست دامنه ها</span>
           </div>
-        </transition>
+        </template>
+        <b-dropdown-item 
+          @click="getName(slide.name)"
+          v-for="slide in slides"
+          :key="slide.id"
+          href="#"
+        >
+          {{ slide.name }}
+        </b-dropdown-item>
+      </b-dropdown>
+    </div>
+
+    <div class="container d-none d-md-block">
+      <div class="domain_list row mt-5">
+        <div class="col-2 align-items-center d-flex justify-content-end">
+          <span @click="domainList" id="demo" class="p-2 action">
+            لیست دامنه ها >
+          </span>
+        </div>
+        <div class="col-8">
+          <transition name="slide-fade">
+            <div v-if="show_domains" class="domian_carousel d-flex">
+              <carousel
+                :paginationEnabled="false"
+                :perPageCustom="[
+                  [480, 2],
+                  [768, 4],
+                ]"
+                class="w-100"
+              >
+                <slide
+                  v-for="slide in slides"
+                  :key="slide.id"
+                  class="carousel_slide px-3 py-2"
+                >
+                  <div @click="getName(slide.name)">
+                    {{ slide.name }}
+                  </div>
+                </slide>
+              </carousel>
+
+              <span class="add_domain px-3 py-2 mx-2"> افزودن دامنه + </span>
+            </div>
+          </transition>
+        </div>
+        <div class="col-2"></div>
       </div>
-      <div class="col-2"></div>
     </div>
   </div>
 </template>
@@ -88,8 +114,9 @@ export default {
     },
     getName(slide_name) {
       var y = document.getElementById("demo");
-      console.log("ihnujhu");
-      y.innerHTML =  slide_name + " > ";
+      var z = document.getElementById("demo1");
+      y.innerHTML = slide_name + " > ";
+      z.innerHTML = slide_name ;
       localStorage.slide_name = slide_name;
       this.show_domains = false;
     },
@@ -100,7 +127,31 @@ export default {
 };
 </script>
 
+<style>
+.dropdown-toggle:focus {
+  box-shadow: none;
+}
+.dropdown-menu {
+  background:rgba(255, 255, 255, 0.6);
+  border-radius:15px;
+  border: none;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
+}
+</style>
+
 <style scoped>
+.bg-blue {
+  background: #6da9c4!important
+}
+.radius-40 {
+  border-radius: 40px;
+}
+.bg-lightgray{
+  background: rgb(233, 233, 233);
+}
+.text-gray {
+  color: rgb(173, 173, 173);
+}
 .add_domain {
   background: #9ad2eb;
   color: #fff;
